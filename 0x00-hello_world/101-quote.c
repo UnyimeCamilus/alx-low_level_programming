3431f8c3c9b76cf53449cd77f882b91ab32e34a1
UnymeCamilus <camilornzu15@gmail.com>
2023-09-09 03:41:27 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A program that print standard error
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
