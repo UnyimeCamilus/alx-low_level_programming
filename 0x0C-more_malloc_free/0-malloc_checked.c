11f99e332860285d1b1a42d7e92c16ad6f29bfe4
UnymeCamilus <camilornzu15@gmail.com>
2023-10-07 03:36:53 -0700

    main.h

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: size of memory
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *output;
+
+	output = malloc(b);
+	if (output == NULL)
+		exit(98);
+	else
+		return (output);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 int _putchar(char c)
-Prototype: void *malloc_checked(unsigned int b);
-Prototype: int *array_range(int min, int max);
-Prototype: char *string_nconcat(char *s1, char *s2, unsigned int n);
-Prototype: void *_calloc(unsigned int nmemb, unsigned int size);
-Prototype: void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *malloc_checked(unsigned int b);
+int *array_range(int min, int max);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
