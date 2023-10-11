eb7b50eb71f6cea9c23b51e3a6b38de251e71f5a
UnymeCamilus <camilornzu15@gmail.com>
2023-10-11 05:06:50 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct that create a dog details
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Desc: first struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
