5f38afe4bf09b3bb944d0990ed8d1e8b3de0971c
UnymeCamilus <camilornzu15@gmail.com>
2023-10-11 21:37:41 -0700

    update file

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,7 +2,7 @@
 #define MACROLIKE_H
 /**
  * File: 3-function_like_macro
- * @x: input value 
+ * @x: input value
  * Desc: header file that compute absolute value
  */
 #define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
