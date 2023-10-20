ada21e3a477858ea79935acdc262c59a7b1f5906
UnyimeCamilus <camilornzu15@gmail.com>
2023-10-20 09:53:54 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+void HundredFirstbyALX(void)__attribute__ ((constructor));
+/**
+ * HundredFirst - Function that print before the main
+ * function is executed
+ */
+void HundredFirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
