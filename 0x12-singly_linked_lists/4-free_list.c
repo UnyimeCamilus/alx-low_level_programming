58fe3bc3164b89a1fa6b3bb7d52a90dde0bb4af1
UnyimeCamilus <camilornzu15@gmail.com>
2023-10-20 09:16:55 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a list_t list
+ * @head: head node is store here
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+	free(head->str);
+	free(head);
+}
